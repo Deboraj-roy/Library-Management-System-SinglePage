fbb977d67f97f88360d424783951555e603d9796
“DEBORAJ <deborajroy123@gmail.com>
2024-09-25 13:42:33 +0600

    New Footer Created

--- src/Front-end-web/src/app/shared/shared.module.ts
+++ src/Front-end-web/src/app/shared/shared.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../material/material.module';
 import { PageHeaderComponent } from './components/page-header/page-header.component';
+import { PageFooterComponent } from './components/page-footer/page-footer.component';
 
 
 
 @NgModule({
   declarations: [
-    PageHeaderComponent
+    PageHeaderComponent,
+    PageFooterComponent
   ],
   imports: [ CommonModule, MaterialModule ],
   exports: [ CommonModule, MaterialModule, PageHeaderComponent ]
