0d3b47e9faa60161e7cc49dfd7027625e2cedd07
“DEBORAJ <deborajroy123@gmail.com>
2024-09-25 12:29:50 +0600

    Page Header ng g c shared/components/PageHeader --no-standalone

--- src/Front-end-web/src/app/app.component.ts
+++ src/Front-end-web/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SharedModule } from './shared/shared.module';
+import { CommonModule } from '@angular/common';
  
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SharedModule],
+  imports: [CommonModule, RouterOutlet, SharedModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
