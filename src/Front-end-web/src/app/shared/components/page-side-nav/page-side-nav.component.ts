2bec474a682bfd12c094a99d0e21545436f2a9d8
“DEBORAJ <deborajroy123@gmail.com>
2024-09-26 15:17:46 +0600

    Basic Structure with interface, value for loop

--- src/Front-end-web/src/app/shared/components/page-side-nav/page-side-nav.component.ts
+++ src/Front-end-web/src/app/shared/components/page-side-nav/page-side-nav.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+export interface NavigationItem {
+  value: string;
+  link: string;
+}
+
 @Component({
   selector: 'page-side-nav',
   templateUrl: './page-side-nav.component.html',
   styleUrl: './page-side-nav.component.scss'
 })
+
 export class PageSideNavComponent {
-panelName: string = 'Student Panel';
+  panelName: string = 'Student Panel';
+  navItems: NavigationItem[] = [];
+
+  /**
+   *
+   */
+  constructor() {
+    this.navItems = [
+      { value: 'View Books', link: 'View-books' },
+      { value: 'My Orders', link: 'my-orders' }
+    ]
+    
+  }
 }
