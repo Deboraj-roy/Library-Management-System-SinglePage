06547a11ed459d1913b6b113f1c6d20f30b04d2a
“DEBORAJ <deborajroy123@gmail.com>
2024-09-26 14:57:42 +0600

    side nav, page not found component added, configure routs

--- src/Front-end-web/src/app/shared/shared.module.ts
+++ src/Front-end-web/src/app/shared/shared.module.ts
@@ -3,15 +3,28 @@ import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../material/material.module';
 import { PageHeaderComponent } from './components/page-header/page-header.component';
 import { PageFooterComponent } from './components/page-footer/page-footer.component';
+import { PageSideNavComponent } from './components/page-side-nav/page-side-nav.component';
+import { RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 
 
 @NgModule({
   declarations: [
     PageHeaderComponent,
-    PageFooterComponent
+    PageFooterComponent,
+    PageSideNavComponent,
+    PageNotFoundComponent
   ],
-  imports: [ CommonModule, MaterialModule ],
-  exports: [ CommonModule, MaterialModule, PageHeaderComponent, PageFooterComponent ]
+  imports: [ CommonModule, MaterialModule, RouterModule ],
+  exports: [
+     CommonModule,
+     MaterialModule, 
+     PageHeaderComponent, 
+     PageFooterComponent,
+     PageSideNavComponent,
+     RouterModule,
+     PageNotFoundComponent
+     ]
 })
 export class SharedModule { }
