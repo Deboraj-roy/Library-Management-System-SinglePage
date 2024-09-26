41717a9b8952e799f28d66a9aab270a15a7b21b1
“DEBORAJ <deborajroy123@gmail.com>
2024-09-26 23:56:38 +0600

    register works!

--- src/Front-end-web/src/app/auth/auth.module.ts
+++ src/Front-end-web/src/app/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -11,7 +12,8 @@ import { LoginComponent } from './login/login.component';
     LoginComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ]
 })
 export class AuthModule { }
