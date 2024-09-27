3b2856f6b6b6f4da2070ddee8a791b54e2f40ff5
“DEBORAJ <deborajroy123@gmail.com>
2024-09-27 12:00:57 +0600

    Login Form

--- src/Front-end-web/src/app/auth/login/login.component.ts
+++ src/Front-end-web/src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'login',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  loginForm: FormGroup;
+  hidePassword: boolean = true;
+
+  /**
+   *
+   */
+  constructor(fb: FormBuilder) {
+        this.loginForm = fb.group({
+          email: fb.control('',[Validators.required]),
+          password: fb.control('',[Validators.required])
+        });
+  }
+
+  login(){
+    let loginInfo = {
+      email: this.loginForm.get('email')?.value,
+      password: this.loginForm.get('password')?.value,
+    }
+  }
 }
