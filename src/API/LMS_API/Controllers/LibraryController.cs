6cb9d9f54ce60e851423d1ec475b9f08d7c79f69
Deboraj-roy <deborajroy123@gmail.com>
2024-10-02 11:22:50 +0600

    Register Enpoint 1

--- src/API/LMS_API/Controllers/LibraryController.cs
+++ src/API/LMS_API/Controllers/LibraryController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using LMS_API.Data;
+using LMS_API.Entities;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LMS_API.Controllers
@@ -7,6 +9,28 @@ namespace LMS_API.Controllers
     [ApiController]
     public class LibraryController : ControllerBase
     {
+        private readonly ApplicationDbContext _context;
+
+        public LibraryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("Register")]
+        public ActionResult Register(User user)
+        {
+            user.AccountStatus = AccountStatus.UNAPROVED;
+            user.UserType = UserType.STUDENT;
+            user.CreatedOn = DateTime.Now;
+
+            _context.Users.Add(user);
+            _context.SaveChanges();
+
+            return Ok(@"
+                        Thanks for registering.
+                        Your Account has been sent for approval.
+                        once approved you can login, you will get an email shortly.");
+        }
 
     }
 }
